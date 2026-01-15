aeac42094f3143a53506dbad1d9ada0e6f92d088
SgAtjiit <shrishpankajguptadbd6@gmail.com>
2026-01-15 18:05:55 +0530

    add user channel profile and watch history features: implement routes and controller methods for fetching user channel profile and watch history

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -14,3 +14,7 @@ const subscriptionSchema = new mongoose.Schema({
 },{timestamps:true})
 
 export const Subscription = mongoose.model("Subscription",subscriptionSchema)
+
+
+//if how many subscribers to a channel - i will count the no of matches i find to the channel in documents
+//if how many channels subscriber by a subscriber - i will count the no of matches i find to that  subscriber in documents
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,31 +1,52 @@
 import { Router } from "express";
-import { loginUser, registerUser,logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateUserDetails, updateUserAvatar, updateUserCoverImage } from "../contollers/user.controller.js";
+import {
+  loginUser,
+  registerUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateUserDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../contollers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-const  router = Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name:"avatar",
-            maxCount:1
-        },
-        {
-            name:"coverImage",
-            maxCount:1
-        }
-    ]),
-    registerUser)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-router.route("/login").post(loginUser)
-
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT,logoutUser)
-router.route("/refreshUser").post(verifyJWT,refreshAccessToken)
-router.route("/changePassword").post(verifyJWT,changeCurrentPassword)
-router.route("/getUser").post(verifyJWT,getCurrentUser)
-router.route("/updateUserDetails").post(verifyJWT,updateUserDetails)
-router.route("/updateAvatar").post(verifyJWT,updateUserAvatar)
-router.route("/updateUserCoverImage").post(verifyJWT,updateUserCoverImage)
-export default router
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refreshUser").post(verifyJWT, refreshAccessToken);
+router.route("/changePassword").post(verifyJWT, changeCurrentPassword);
+router.route("/getCurrentUser").post(verifyJWT, getCurrentUser);
+//patch is used when only a few details need to be updated
+//while put is used when full object needs to be updated
+router.route("/updateUserDetails").patch(verifyJWT, updateUserDetails);
+router
+  .route("/updateAvatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/updateUserCoverImage")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+//since we need data from params username must be as it is as we are writing,while extracting it from params in controller
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/getWatchHistory").get(verifyJWT, getWatchHistory);
+export default router;
